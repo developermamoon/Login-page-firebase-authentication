bdb372505cde8b90749e92df169e9e34eca8e247
Md. Abdullah Al-Mamoon <almamoon18@gmail.com>
2022-10-20 21:07:32 +0600

    updated

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,18 +1,21 @@
 import Modal from "react-bootstrap/Modal";
-import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
 import app from '../../firebase/firebase.init';
+import User from "../User/User";
 
 const auth = getAuth(app);
 
 
 const Login = () => {
 
+    const [loginUser, setLoginUser] = useState({})
     const [showError, setShowError] = useState('');
     const [show, setShow] = useState(false);
+    const [showPage, setShowPage] = useState(true)
 
     const handleClose = () => setShow(false);
     const handleShow = () => {
@@ -34,8 +37,11 @@ const Login = () => {
         signInWithEmailAndPassword(auth, email, password)
         .then(result => {
             const user = result.user;
-            console.log(user);
+            setLoginUser(user);
+            setShowPage(false);
+            // console.log(user);
             form.reset();
+            setShowError("Login Successfull !!")
         })
         .catch(error => {
             console.error("Error: ", error);
@@ -70,78 +76,101 @@ const Login = () => {
             }
         })
     }
+
+    //signout code -----------------------------
+    const manageSignOut = ()=>{
+        signOut(auth)
+            .then(() => {
+                //sign out successful
+                console.log("Sign Out Successfully");
+                setShowPage(true)
+            })
+            .catch((error) => {
+                //error
+            })
+    }
     
 
 
 
 
     return (
-        <div className='w-50 mx-auto border border-3 border-primary p-4 rounded-3'>
-            <Form onSubmit={handleLogin}>
-                <h3 className='text-primary fw-bold text-center'>Login Page</h3>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control name='email' type="email" placeholder="Enter email" required/>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control name='password' type="password" placeholder="Password" required />
-                </Form.Group>
-                <p className='text-danger'><small>{showError}</small></p>
-                <div className='d-flex align-items-center justify-content-between'>
-                    <p><small>Don't have an account? <Link to='/register'>Sign Up</Link></small></p>
-                    
-
-                    {/*----- pop up code for forget password -----*/}
-                    
-                    
-                    <p>
-                    <Button variant="btn btn-link" onClick={handleShow}>
-                        <small>Forget Password</small>
-                    </Button>
-                    </p>
-
-                    <Modal show={show} onHide={handleClose}>
-                        <Modal.Header closeButton>
-                            <Modal.Title>Modal heading</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
+        <div>
+            {showPage && <div className='w-50 mx-auto border border-3 border-primary p-4 rounded-3'>
+                <Form onSubmit={handleLogin}>
+                    <h3 className='text-primary fw-bold text-center'>Login Page</h3>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control name='email' type="email" placeholder="Enter email" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control name='password' type="password" placeholder="Password" required />
+                    </Form.Group>
+                    <p className='text-danger'><small>{showError}</small></p>
+                    <div className='d-flex align-items-center justify-content-between'>
+                        <p><small>Don't have an account? <Link to='/register'>Sign Up</Link></small></p>
 
 
-                            <Form onSubmit={forgetEmail}>
-                                <Form.Group className="mb-3" controlId="">
-                                    <Form.Label>Email</Form.Label>
-                                    <Form.Control name='mail' type="email" placeholder="Enter email" required />
-                                </Form.Group>
+                        {/*----- pop up code for forget password -----*/}
 
-                                {<p className="text-success">{mailSent}</p>}
-                                <Button variant="primary" disabled={isDisabled} type="submit" >
-                                    Submit
-                                </Button>
-                            </Form>
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button variant="secondary" onClick={handleClose}>
-                                Close
+
+                        <p>
+                            <Button variant="btn btn-link" onClick={handleShow}>
+                                <small>Forget Password</small>
                             </Button>
-                            
-                        </Modal.Footer>
-                    </Modal>
-                    
+                        </p>
 
-                    
+                        <Modal show={show} onHide={handleClose}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Modal heading</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+
+
+                                <Form onSubmit={forgetEmail}>
+                                    <Form.Group className="mb-3" controlId="">
+                                        <Form.Label>Email</Form.Label>
+                                        <Form.Control name='mail' type="email" placeholder="Enter email" required />
+                                    </Form.Group>
+
+                                    {<p className="text-success">{mailSent}</p>}
+                                    <Button variant="primary" disabled={isDisabled} type="submit" >
+                                        Submit
+                                    </Button>
+                                </Form>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="secondary" onClick={handleClose}>
+                                    Close
+                                </Button>
+
+                            </Modal.Footer>
+                        </Modal>
+
+
+
+                    </div>
+                    <Button variant="primary" type="submit">
+                        Login
+                    </Button>
+
+                </Form>
+
+                <div className='mt-3 d-flex flex-column align-items-center'>
+                    <button className='btn btn-danger w-50 mt-2'>Signup with Google </button>
+                    <button className='btn btn-success w-50 mt-2'>Signup with GitHub </button>
+                    <button className='btn btn-primary w-50 mt-2'>Signup with Facebook </button>
                 </div>
-                <Button variant="primary" type="submit">
-                    Login
-                </Button>
-                
-            </Form>
+            </div>}
 
-            <div className='mt-3 d-flex flex-column align-items-center'>
-                <button className='btn btn-danger w-50 mt-2'>Signup with Google </button>
-                <button className='btn btn-success w-50 mt-2'>Signup with GitHub </button>
-                <button className='btn btn-primary w-50 mt-2'>Signup with Facebook </button>
-            </div>
+            {!showPage && <div className="container text-center mt-5">
+                <User data={loginUser}></User>
+                <button className="btn btn-danger" onClick={manageSignOut}>LogOut</button>
+            </div>}
+
+            
+            {/* {console.log(loginUser)} */}
             
         </div>
     );
---
+++ src/Components/User/User.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const User = (data) => {
+    const { displayName, email, photoURL} = data.data;
+    // console.log(data.data);
+    return (
+        <div>
+            <h2>Welcome!! {displayName}</h2>
+            <p><b>Your email Address is:  </b>{email}</p>
+            <img src={photoURL} alt="" />
+        </div>
+    );
+};
+
+export default User;
